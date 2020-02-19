93bfe3106a08f3fc6eb3ee2bea677adb32da094e
Colin Littlejohn <crazycodgie13@gmail.com>
2020-02-19 13:59:59 -0800

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import React, { useState } from "react"
 import "./App.css"
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0)
+  const [grammar, setGrammar] = useState("clicks")
   function handleCount(e) {
     e.preventDefault()
     setCount(count + 1)
-  }
-
-  const Click = () => {
-    const [Click, setClicks] = useState("Click")
-    return <p onClicks={() => setClicks("Click")}>{Click}</p>
+    setGrammar(grammar === 0 ? "click" : "clicks")
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <button type="button" onClick={handleCount} id="like">
-          {count}
+        <button type="button" onClick={handleCount} id="click">
+          {count} {grammar}
         </button>
-        <p>Clicks</p>
       </header>
     </div>
   )
 }
-
-export default App
